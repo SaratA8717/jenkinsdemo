a907354fa5be601725456eca856c820ddf750613
Sarat A <sarat.ankala@outlook.com>
2024-05-01 16:49:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 terraform {
   required_version = ">= 1.5.7"
-  # backend "azurerm" {
-  #   resource_group_name  = "tfstate8739"
-  #   storage_account_name = "tfstor9839"
-  #   container_name       = "jenkinstfstate"
-  #   key                  = "jenkinstfstate.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "tfstate8739"
+    storage_account_name = "tfstor9839"
+    container_name       = "jenkinstfstate"
+    key                  = "jenkinstfstate.tfstate"
+    access_key = "HGx1gat7UHLSrLomfAec+vUfVCzlKyq+G9++jYpzClaHWcGhYKR7KSnocgvx1unTQ+lqy5qTo+vD+AStLB6K5Q=="
+  }
 }
 
 provider "azurerm" {
@@ -15,6 +16,6 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "testrg" {
-  name     = "github-sagar-rg"
+  name     = "github-sarat-rg"
   location = "eastus"
 }
